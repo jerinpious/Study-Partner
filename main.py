334d7b2a07755e3e10ffdee1466a91439eafce29
jerinpious <jpious4925@conestogac.on.ca>
2024-03-22 12:24:48 -0400

    pages and flask routes

--- main.py
+++ main.py
@@ -1,32 +1,60 @@
 from googletrans import Translator
 import pyttsx3
+from flask import Flask
+import uuid
+import os
+from flask import render_template,request
 
 
 tts = pyttsx3.init()
 
 
-def texttospeech(text):
+def texttospeech(text,record):
     tts.say(text)
-    tts.runAndWait()
+    tts.save_to_file(text,record)
+
 
 def translator(text, language):
     translator = Translator()
     translated = translator.translate(text,dest=language)
     return translated.text
 
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return render_template('home.html')
 
-languages=['en','fr','nl','ar','de']
+@app.route("/translate", methods=['GET','POST'])
+def translate():
+    
+    translated = ""
+    if request.method=="POST":
+        text = request.form.get("text_to_transalte")
+        
+        translated=translator(text,'fr')
+        
+        
+    return render_template('translate.html', translated=translated)
 
-print("\nChoose the language to translate into..")
-print("1. English\n2. French\n3. Dutch\n4. Arabic\n5. German\n")
-choice = int(input("Choose an option: "))
-language = languages[choice-1]
-text = input("What would you like to translate>> ")
+@app.route("/speech",methods=['POST','GET'])
+def speech():
+    
+    if request.method == "POST":
+        text = request.form.get("text_to_speech")
+        filename = str(uuid.uuid4()) + ".mp3"
+        record_path = os.path.join("static", "records", filename)
+        
+        # Save the translated text to audio file
+        texttospeech(text, record_path)
+        # Render template with link to download the audio file
+        return render_template('speech.html', audio_file=record_path)
+    else:
+        return render_template('speech.html')
 
-output = translator(text, language)
+if __name__ == '__main__':
+    app.run(debug=True)
 
-texttospeech(output)
+# languages=['en','fr','nl','ar','de']
 
-print(output)
 
 
